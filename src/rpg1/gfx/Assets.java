d1118435210104363a7acdf483887a8a75fd1ff4
MichalRzepecki97 <michalrzepecki97@gmail.com>
2018-09-19 12:22:14 +0200

    Assets

--- src/rpg1/gfx/Assets.java
+++ src/rpg1/gfx/Assets.java
@@ -10,11 +10,11 @@ public class Assets {
     public static void init(){
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/spriteSheet.png"));
 
-        player = sheet.crop(0,0, width,height);
-        dirt = sheet.crop(width,0, width,height);
-        grass = sheet.crop(width*2,0,width,height);
-        stone = sheet.crop(width*3,0,width,height);
-        tree = sheet.crop(0,height,width,height);
+        dirt = sheet.crop(0,0, width,height);
+        grass = sheet.crop(width,0, width,height);
+        tree = sheet.crop(width*2,0,width,height);
+        player = sheet.crop(width*3,0,width,height);
+        stone = sheet.crop(width*4,0,width,height);
 
 
     }
