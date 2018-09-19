7f2aa59207459a36e2ef0a5fee3aaddb8854e6ab
MichalRzepecki97 <michalrzepecki97@gmail.com>
2018-09-19 12:44:58 +0200

    fps

--- src/rpg1/Game.java
+++ src/rpg1/Game.java
@@ -25,7 +25,10 @@ public class Game implements Runnable {
         display = new Display(Title,Width,Height);
         Assets.init();
     }
+    int x =0;
+
     private void tick(){
+        x += 1;
 
     }
     private void render(){
@@ -39,18 +42,41 @@ public class Game implements Runnable {
         g.clearRect(0,0,Width,Height);
         //draw
 
-        g.drawImage(Assets.grass,10,10,null);
+        g.drawImage(Assets.dirt,x,10,null);
         bs.show();
         g.dispose();
     }
     @Override
     public void run() {
         init();
+//ograniczenie klatek na sekunde
+        int fps = 60;
+        double timePerTick = 1000000000 / fps;
+        double delta = 0;
+        long now;
+        long lasteTime = System.nanoTime();
+        long timer = 0;
+        int ticks =0;
+
 
         while (running){
-        tick();
-        render();
+            now = System.nanoTime();
+            delta += (now -lasteTime) / timePerTick;
+            timer +=now -lasteTime;
+            lasteTime = now;
 
+            if (delta >= 1) {
+                tick();
+                render();
+                ticks++;
+                delta --;
+                //koniec klatek
+            }
+            if(timer >= 1000000000){
+                System.out.println("fps: " +ticks);
+                ticks = 0;
+                timer = 0;
+            }
         }
         stop();
     }
