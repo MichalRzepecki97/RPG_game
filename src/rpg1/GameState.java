e6914d52faea5dd58994b35e9c54d0e202c4f90c
MichalRzepecki97 <michalrzepecki97@gmail.com>
2018-09-28 16:07:39 +0200

    Basics of Map

--- src/rpg1/GameState.java
+++ src/rpg1/GameState.java
@@ -1,30 +1,35 @@
 package rpg1;
 
 
+
 import rpg1.entities.Player;
 import rpg1.tiles.Tile;
+import world.Map;
 
 import java.awt.*;
 
 public class GameState extends State {
 
     private Player player;
+    private Map map;
 
 
     public GameState(Game game ) {
         super(game);
         player = new Player(game,100,100);
+        map = new Map("");
     }
 
     @Override
     public void tick() {
-
+        map.tick();
         player.tick();
     }
 
     @Override
     public void render(Graphics g) {
+        map.render(g);
         player.render(g);
-        Tile.tiles[0].render(g,0,0);
+        Tile.tiles[2].render(g,0,0);
     }
 }
